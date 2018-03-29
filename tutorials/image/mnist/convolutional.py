92c2849d3b9a945b3c0b9b239dd584f23ecdbe57
gaolining <gaolining2006@126.com>
2018-03-29 19:12:54 +0800

    add new files

--- tutorials/image/mnist/convolutional.py
+++ tutorials/image/mnist/convolutional.py
@@ -117,6 +117,7 @@ def error_rate(predictions, labels):
       numpy.sum(numpy.argmax(predictions, 1) == labels) /
       predictions.shape[0])
 
+
 def save_model(sess):
     """
     :return:
@@ -125,11 +126,13 @@ def save_model(sess):
     saver.save(sess, "./model.ckpt")
     tf.train.write_graph(sess.graph_def, "/home/extend/code/models/tutorials/image/mnist/", "graph.pb",as_text=False)
 
+
 def save_model_pb(sess):
     builder = tf.saved_model.builder.SavedModelBuilder("./savedmodel")
     builder.add_meta_graph_and_variables(sess,[tf.saved_model.tag_constants.TRAINING])
     builder.add_meta_graph([tf.saved_model.tag_constants.SERVING])
     builder.save()
+
 
 def main(_):
   if FLAGS.self_test:
